2dc1c9cf0f9f01b6670f85288c7df4030b45edb8
I WAYAN RAKA PUTRA YANA <rakaputray20@gmail.com>
2025-05-12 09:37:38 +0800

    update: techstack section in home page

--- app/page.tsx
+++ app/page.tsx
@@ -3,12 +3,14 @@ import About from "@/components/HomePage/About/About";
 import Service from "@/components/HomePage/Service/Service";
 import Faq from "@/components/HomePage/Faq/Faq";
 import Contact from "@/components/HomePage/Contact/Contact";
+import TechStack from "@/components/HomePage/TechStack/TechStack";
 
 export default function HomePage() {
   return (
     <main className="container mx-auto max-w-7xl px-6">
       <Hero />
       <About />
+      <TechStack />
       <Service />
       <Faq />
       <Contact />
